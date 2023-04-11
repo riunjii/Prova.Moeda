c43ef78330b79fa07a63c6620080538a04166559
riunjii <vitor2002_souza@hotmail.com>
2023-04-11 10:46:13 -0300

    6  commit

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -9,10 +9,24 @@ import { Component } from '@angular/core';
 export class ConversorComponent {
 moeda1!: number;
 resp!: number;
+moeda2!:number;
+holder:string = 'Resultado';
 
+converter(){
+const moedaCalculada: number = parseInt(this.holder);
 
-
-converter()
+if(moedaCalculada == 1){
+  this.resp = this.moeda1 * this.moeda2;
+}
+else if(moedaCalculada == 2){
+  this.resp = this.moeda1 * this.moeda2;
+}
+else if(moedaCalculada == 3){
+  this.resp = this.moeda1 * this.moeda2;
+}
+else{
+  alert("Informe os valores")
+}
 {
 
 }
@@ -20,4 +34,4 @@ converter()
 }
 
 
-
+}
